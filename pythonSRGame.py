9ee274454eec7ac374674cc049ca86220cb1e539
markgill <G00348787@gmit.ie>
2020-04-07 20:29:48 +0100

    added more comments

--- pythonSRGame.py
+++ pythonSRGame.py
@@ -35,10 +35,12 @@ def Game():
         color = random.choice(font_colours)
         text = random.choice(font_colours)
         font = random.choice(fonts)
+        # saying the background colour was going to be objectiveDecider==3 but adding a filled-in background made the text almost invisible
         background = random.choice(bg_colours)
         if color != background:
             f = Figlet(font)
             print(fg(color) + f.renderText(text))
+            # objectiveDecider decides whether you have to say the word or colour
             objectiveDecider = random.randint(1, 2)
             # print(objectiveDecider)
             # print(font)
@@ -46,44 +48,58 @@ def Game():
             if objectiveDecider == 1:
                 with microphone as source:
                     print(attr("reset") + "Say the colour of the text")
+                    # audio is the data passed back from the microphone
                     audio = r.listen(source, phrase_time_limit=2)
                     try:
+                        # its then sent to be recognized
                         guess = r.recognize_google(audio)
+                        # takes first word said
                         guess = guess.split(" ")[0]
                         if guess.lower() == color:
                             print(fg("green") + "Correct")
                             score += 1
+                        # say any of these words to leave during a game
                         elif guess.lower() == "quit" or guess.lower() == "close" or guess.lower() == "leave":
                             sys.exit()
+                        # anything thats not correct, is wrong
                         else:
                             print(fg("red") + "Wrong")
                             print(fg("red") + "Correct answer was: "+ color + ". You said : " +guess)
 
                             wrong_guesses += 1
                         time.sleep(2)
+                    # unrecognizable audio
                     except sr.UnknownValueError:
                         print("Google Speech Recognition could not understand audio")
+                    # Google service is unreachable
                     except sr.RequestError as e:
                         print("Could not request results from Google Speech Recognition service; {0}".format(e))
             elif objectiveDecider == 2:
                 with microphone as source:
                     print(attr("reset") + "Say the word!")
+                    # audio is the data passed back from the microphone
                     audio = r.listen(source, phrase_time_limit=2)
                     try:
+                        # its then sent to be recognized
                         guess = r.recognize_google(audio)
+                        # takes first word said
                         guess = guess.split(" ")[0]
                         if guess.lower() == text:
                             print(fg("green") + "Correct")
                             score += 1
+                        # say any of these words to leave during a game
                         elif guess.lower() == "quit" or guess.lower() == "close" or guess.lower() == "leave":
                             sys.exit()
+                        # anything thats not correct, is wrong
                         else:
                             print(fg("red") + "Wrong")
                             print(fg("red") + "Correct answer was: "+ color + ". You said : " +guess)
                             wrong_guesses += 1
                         time.sleep(2)
+                    # unrecognizable audio
                     except sr.UnknownValueError:
                         print("Google Speech Recognition could not understand audio")
+                    # Google service is unreachable
                     except sr.RequestError as e:
                         print("Could not request results from Google Speech Recognition service; {0}".format(e))
             if wrong_guesses == 2:
@@ -96,8 +112,11 @@ def Game():
         time.sleep(4)
         with microphone as source:
             print(attr("reset") + "Yes or no?")
+            # audio is the data passed back from the microphone
             audio = r.listen(source, phrase_time_limit=3)
+            # its then sent to be recognized
             decision = r.recognize_google(audio)
+            # takes first word of recording
             decision = decision.split(" ")[0]
             if decision == "yes" or decision == "Yes":
                 print(fg("green") + "Get Ready")
@@ -114,8 +133,8 @@ def Game():
 
 def Main():
     with microphone as source:
+        # initial start to game
         print(attr("reset") + "Welcome to the colour aptitude test")
-        #time.sleep(2)
         print(attr("reset") + "You will be prompted to either say the colour of the text displayed, or the actual "
                               "word itself")
         #time.sleep(5)
@@ -125,8 +144,11 @@ def Main():
         time.sleep(2)
         try:
             print("Answer now!")
+            # audio is the data passed back from the microphone
             audio = r.listen(source, phrase_time_limit=3)
+            # its then sent to be recognized
             decision = r.recognize_google(audio)
+            # takes first word of recording
             decision = decision.split(" ")[0]
             if decision == "yes" or decision == "Yes":
                 print(fg("green") + "Get Ready")
@@ -136,8 +158,10 @@ def Main():
             else:
                 print(fg("red") + "Bye")
                 sys.exit()
+        # unrecognizable audio
         except sr.UnknownValueError:
            print("Google Speech Recognition could not understand audio")
+        # Google service is unreachable
         except sr.RequestError as e:
            print("Could not request results from Google Speech Recognition service; {0}".format(e))
     Game()
